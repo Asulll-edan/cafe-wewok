b415dfbd648573ed1a7da312e218fb94eba34ffd
azka024 <azzakiyah.024@gmail.com>
2025-05-27 14:49:47 +0700

    arya dan daffa

---
+++ Maincourse.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Main Course</title>
+  <link rel="stylesheet" href="Combo.css">
+</head>
+<body>
+  <div class="menu-container">
+    <div class="menu-header">MAIN COURSE</div>
+    <div class="MC-grid">
+
+      <!-- TEMPLATE ITEM (ganti src, nama, dan deskripsi sesuai kebutuhan) -->
+         <!-- ITEM 1 -->
+      <div class="MC-item">
+        <img src="nasgors.jfif" alt="">
+        <div class="MC-name">Nasi Goreng Spesial</div>
+        <div class="MC-desc">Nasi Goreng Spesial lengkap dengan rempah dan telur.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+         <!-- ITEM 2 -->
+      <div class="MC-item">
+        <img src="nagsorp.jfif" alt="">
+        <div class="MC-name">Nasi Ayam Geprek</div>
+        <div class="MC-desc">Nasi putih dengan daging Ayam Geprek dilumuri sambal.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+         <!-- ITEM 3 -->
+      <div class="MC-item">
+        <img src="sambalm.jfif" alt="">
+        <div class="MC-name">Nasi Ayam Sambal Matah</div>
+        <div class="MC-desc">Nasi putih dengan daging Ayam yang dilumuri sambal matah.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+         <!-- ITEM 4 -->
+      <div class="MC-item">
+        <img src="sbolog.jfif" alt="">
+        <div class="MC-name">Spaghetti Bolognese</div>
+        <div class="MC-desc">Spaghetti yang di sajikan dengan saus Bolognese.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+         <!-- ITEM 5 -->
+      <div class="MC-item">
+        <img src="B & F.avif" alt="">
+        <div class="MC-name">Burger with Fries</div>
+        <div class="MC-desc">Burger dengan kentang goreng.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+         <!-- ITEM 6 -->
+      <div class="MC-item">
+        <img src="M & C.webp" alt="">
+        <div class="MC-name">Mac & Cheese</div>
+        <div class="MC-desc">Pasta Makaroni yang dimasak dengan saus keju.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+         <!-- ITEM 7 -->
+      <div class="MC-item">
+        <img src="katsu.jpg" alt="">
+        <div class="MC-name">Chicken Katsu</div>
+        <div class="MC-desc">Ayam goreng dibalut tepung roti dengan nasi putih.</div>
+        <button class="fancy-cart-button">Belanj
+        </button>
+      </div>
+
+         <!-- ITEM 8 -->
+      <div class="MC-item">
+        <img src="teriyaki b.webp" alt="">
+        <div class="MC-name">Beef Teriyaki Don</div>
+        <div class="MC-desc">Daging sapi yang diiris tipis dengan saus teriyaki.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+         <!-- ITEM 9 -->
+      <div class="MC-item">
+        <img src="bibimbap.jpg" alt="">
+        <div class="MC-name">Bibimbap</div>
+        <div class="MC-desc">Semangkuk nasi putih dengan sayur-sayuran, daging sapi, telur dan saus pedas gochujang.</div>
+        <button class="fancy-cart-button">Belanja
+        </button>
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
